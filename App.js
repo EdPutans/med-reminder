c5e0ff89f80a5c04d02a1e4694a5cd9042a7063a
Edgar <edputans@gmail.com>
2019-01-22 18:03:32 +0000

    pushing as is since linux does not like RNFS without xcode

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, Title } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import RNFS from 'react-native-fs'
 
 export default class App extends React.Component {
+
+
+  state = {
+    adding: false,
+    // reminders: allreminders.reminders
+  }
+
+  componentDidMount(){
+   RNFS.readFile('./reminders.txt', 'utf-8')
+    .then(res=> console.log({res}))
+    // console.log({reminders: this.state.reminders})
+  }
+
+  toggleAdd = () => {
+    this.setState({adding: !this.state.adding})
+    console.log(this.state)
+    let dateObj = {time: new Date('January 22, 2019 18:00:00')}
+
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
+      <Text style={styles.whiteTitle}>
+            {this.state.reminders && "Henlo"}
+        </Text>
         <Text style={styles.whiteTitle}>
             Medrem
         </Text>
+        {
+          // (this.state.reminders.map(d=> 
+          // <Text>
+          //   {d.time.toString()})
+          // </Text>))
+        }
+        <Button 
+          onPress={this.toggleAdd} 
+          title="+"
+        />
       </View>
     );
   }
@@ -22,7 +57,7 @@ const styles = StyleSheet.create({
   },
 
   whiteTitle: {
-    fontSize: 20,
+    fontSize: 50,
     color: '#fff',
   }
 });
---
+++ Components/ReminderForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import reminders from '../reminders.json'
+
+
+export default class ReminderForm extends React.Component {
+
+
+
+    state={
+
+    }
+
+    
+
+    render() {
+        return (
+            <View />
+        )
+    }
+}
